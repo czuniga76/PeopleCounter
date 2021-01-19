00ea0cdd91bdea15c4891c3bb7e2c9b2e7656429
czuniga76 <christian.zuniga@hotmail.com>
2021-01-18 21:25:45 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import sys
+
+sys.path.append("C:\Program Files (x86)\Intel\openvino_2021.2.185")
+sys.path.append("C:\Program Files (x86)\Intel\openvino_2021.2.185\deployment_tools")
+import opencv as cv
+#from inference_engine import IECore, IENetwork
+
+
+print("opencv  ver ") # opencv.__version__)
